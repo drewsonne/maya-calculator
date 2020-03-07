04a7486e0af9bcc4660712781ba281c57a4c766b
Drew <drew.sonne@blis.com>
2020-03-07 11:54:31 +0000

    Add working test harness

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-react',
-    '@babel/preset-es2015'
-  ],
-  plugins: ['@babel/plugin-transform-modules-umd']
-};
